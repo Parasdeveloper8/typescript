c0be7952a7c9a736d0db9e9bf11e12030c62d7a2
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-02-19 23:14:42 +0530

    enums learned

---
+++ 02Basics/enums.js
@@ -0,0 +1,15 @@
+"use strict";
+//enum = enumerations
+Object.defineProperty(exports, "__esModule", { value: true });
+//Enum is like group of variables
+var Ranger;
+(function (Ranger) {
+    Ranger["redRanger"] = "troy";
+    Ranger["blueRanger"] = "b";
+    Ranger["greenRanger"] = "jacke";
+    Ranger["yellowRanger"] = "Gia";
+    Ranger["pinkRanger"] = "ema";
+    Ranger["silverRanger"] = "Orion";
+})(Ranger || (Ranger = {}));
+var ranger1st = Ranger.redRanger;
+var data = "10" /* Student.Class */;
---
+++ 02Basics/enums.ts
@@ -0,0 +1,21 @@
+//enum = enumerations
+
+//Enum is like group of variables
+enum Ranger {
+    redRanger = "troy",
+    blueRanger = "b",
+    greenRanger = "jacke",
+    yellowRanger = "Gia",
+    pinkRanger = "ema",
+    silverRanger = "Orion"
+}
+
+const ranger1st = Ranger.redRanger;
+
+const enum Student {
+     Class = "10",
+     Section = "E-2"
+}
+
+const data = Student.Class;
+export {}
