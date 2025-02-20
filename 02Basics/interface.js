b5259aaa4e826422779f7f95845de7308b247734
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-02-20 16:42:17 +0530

    day  5 : type v/s interface

--- 02Basics/interface.js
+++ 02Basics/interface.js
@@ -1,6 +1,7 @@
+"use strict";
 //interface contains methods with no implementation
+Object.defineProperty(exports, "__esModule", { value: true });
 var paras = { class: 10, moniter: false, name: "paras", routine: function () {
         console.log("Wake up at 6:20 AM");
         console.log("Do exercise");
-    }
-};
+    }, id: "abcd4u" };
--- 02Basics/interface.ts
+++ 02Basics/interface.ts
@@ -10,5 +10,21 @@ interface Student{
 const paras:Student = {class:10,moniter:false,name:"paras",routine() {
        console.log("Wake up at 6:20 AM");
        console.log("Do exercise");
-}
+},id:"abcd4u"
 };
+
+//reopening interface
+interface Student {
+    id:string
+}
+
+//inheritence in interface
+interface StudentTwo extends Student{
+    role:string
+}
+
+//difference b/w type aliases and interface
+//interface can be opened again but not type aliases
+//interface can contain methods in it but type aliases can't
+//interface supports inheritence but not type aliases
+export {}
